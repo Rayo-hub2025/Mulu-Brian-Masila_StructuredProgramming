feeee68edfacb88e01e01ce335ea782f25ad4b99
Rayo-hub2025 <mulubrian274@gmail.com>
2025-11-18 21:58:55 +0300

    Add files via upload
    Assignment 1 Structural Programming

---
+++ C Programs/sphere surface area.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define PI 3.14159   // Defining PI constant
+
+int main() {
+    float radius, surfaceArea;
+
+    // Input
+    printf("Enter the radius of the sphere: ");
+    scanf("%f", &radius);
+
+    // Calculation
+    surfaceArea = 4 * PI * radius * radius;
+
+    // Output
+    printf("Surface Area of the sphere = %.2f\n", surfaceArea);
+
+    return 0;
+}
