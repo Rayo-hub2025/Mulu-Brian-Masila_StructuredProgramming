fb953e8fef995e4a61c08b0537f386efd5a04912
Rayo-hub2025 <mulubrian274@gmail.com>
2025-12-17 12:34:54 +0300

    Add files via upload

---
+++ Pointers/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int num = 10;      // integer variable
+    int *ptr;          // integer pointer
+
+    ptr = &num;        // store address of num in ptr
+
+    printf("Value of num: %d\n", num);
+    printf("Value stored in ptr (address of num): %p\n", ptr);
+    printf("Address of num: %p\n", &num);
+    printf("Value accessed using *ptr: %d\n", *ptr);
+
+    return 0;
+}
